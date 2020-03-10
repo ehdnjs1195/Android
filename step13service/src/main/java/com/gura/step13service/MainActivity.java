da147d980d0c03c101867375e99f12a76adc151f
ADW <ehdnjs1195@naver.com>
2020-03-10 10:09:42 +0900

    [Acorn] Step13Service 핸들러를 이용해 Service 작동원리 파악하기

--- step13service/src/main/java/com/gura/step13service/MainActivity.java
+++ step13service/src/main/java/com/gura/step13service/MainActivity.java
@@ -2,7 +2,9 @@ package com.gura.step13service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+    //서비스 시작 버튼을 눌렀을 때
+    public void start(View v){
+        //서비스를 시작시킬 Intent 객체 생성
+        Intent intent=new Intent(this, MyService.class);
+        //Intent 객체를 이용해서 서비스를 시작 시킨다.
+        startService(intent);   //activity의 startService() 라는 메서드를 호출해서 service에 인텐트 객체를 전달해서 서비스를 시작시킨다.
+    }
+    //서비스 종료 버튼을 눌렀을 때
+    public void end(View v){
+        Intent intent=new Intent(this, MyService.class);
+        //Intent 객체를 이용해서 서비스를 종료 시킨다.
+        stopService(intent);
+    }
+}   // => 서비스를 시작, 종료시킬 때 Intent 객체가 필요하다!!
--- step13service/src/main/java/com/gura/step13service/MyService.java
+++ step13service/src/main/java/com/gura/step13service/MyService.java
@@ -18,7 +18,7 @@ import androidx.annotation.Nullable;
     3. 추가로 필요한 메서드를 오버라이딩해서 작업한다.
 
  */
-public class MyService extends Service {
+public class MyService extends Service {    //서비스가 시작된다는 것은 결국 onStartCommand() 메서드가 호출된다는 것.
 
     //특정 Activity 와 연결이 되었을 때 호출되는 메서드 ( 이 예제에서는 할 작업이 없다.)
     @Nullable
@@ -36,7 +36,9 @@ public class MyService extends Service {
 
     //서비스에서 수행해야할 main 작업을 하는 메서드
     @Override   //객체가 생성되고나서 실제로 사용이 될 때 호출. =>핵심 작업
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    public int onStartCommand(Intent intent, int flags, int startId) {  //특정 시점에 intent 객체를 통해 service가 시작된다. 전달되는 intent가 파라미터로 들어온다.)
+        //핸들러에 메세지 보내기
+        handler.sendEmptyMessage(0);
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -44,6 +46,8 @@ public class MyService extends Service {
     @Override   //액티비티가 종료되고 마무리 작업을 할 때 호출.
     public void onDestroy() {
         super.onDestroy();
+        //핸들러 동작 하지 않도록
+        handler.removeMessages(0);
     }
 
     //카운트값을 저장할 필드
@@ -52,12 +56,13 @@ public class MyService extends Service {
     //핸들러(주기적으로 토스트를 띄울)
     Handler handler = new Handler() {  //(중괄호를 열고 닫아서 익명 클래스를 상속받는 개념)
         @Override
-        public void handleMessage(@NonNull Message msg) {
+        public void handleMessage(@NonNull Message msg) {   //핸들러 객체를 생성하면 자동으로 호출됨.
             count ++;
-            Toast.makeText(getApplicationContext(),count+" 번 호출", Toast.LENGTH_SHORT).show();
-            handler.sendEmptyMessageDelayed(0,5000);
+            Toast.makeText(getApplicationContext(),count+" 번 호출", Toast.LENGTH_SHORT).show();   //getApplicationContext() => Service의 메서드. 부모로부터 물려받은 context를 리턴함.
+            handler.sendEmptyMessageDelayed(0,5000);    //5초 후에 다시 메세지를 보낸다. 즉, 메세지를 한 번 보내면 계속 5초마다 메세지를 보내게 된다. (handleMessage() 메서드 호출)
         }
     };
 
 
 }
+// onCreate()만들기. -> onStartCommand()시작. -> onDestroy()종료.
